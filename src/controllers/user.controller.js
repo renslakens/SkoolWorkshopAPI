4afe304f58a59d086de5c586c868dda5699f1e6e
Rens Lakens <rens@lakens.org>
2022-05-31 13:52:49 +0200

    alleen de benodigde velden aangepast voor de user

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -7,35 +7,35 @@ const jwtSecretKey = require('../config/config').jwtSecretKey
 let controller = {
     validateUser: (req, res, next) => {
         let user = req.body;
-        let { firstName, lastName, emailAddress, password, nationality, sex, birhtDate, birthCity, birthCountry, phoneNumber, street, postalCode, city, country } = user;
+        let { firstName, lastName, emailAddress, password } = user;
         try {
             assert(typeof firstName === 'string', 'The firstname must be a string');
             assert(typeof lastName === 'string', 'The lastName must be a string');
             assert(typeof emailAddress === 'string', 'The emailAddress must be a string');
             assert(typeof password === 'string', 'The password must a string');
-            assert(typeof nationality === 'string', 'The nationality must be a string');
-            assert(typeof sex === 'string', 'The sex must be a string');
-            assert(typeof birhtDate === 'string', 'The birthDate must be a string');
-            assert(typeof birthCity === 'string', 'The birthCity must be a string');
-            assert(typeof birthCountry === 'string', 'The birthCountry must be a string')
-            assert(typeof street === 'string', 'The street must be a string');
-            assert(typeof postalCode === 'string', 'The postalCode must be a string');
-            assert(typeof city === 'string', 'The city must be a string');
-            assert(typeof country === 'string', 'The country must be a string');
+            // assert(typeof nationality === 'string', 'The nationality must be a string');
+            // assert(typeof sex === 'string', 'The sex must be a string');
+            // assert(typeof birhtDate === 'string', 'The birthDate must be a string');
+            // assert(typeof birthCity === 'string', 'The birthCity must be a string');
+            // assert(typeof birthCountry === 'string', 'The birthCountry must be a string')
+            // assert(typeof street === 'string', 'The street must be a string');
+            // assert(typeof postalCode === 'string', 'The postalCode must be a string');
+            // assert(typeof city === 'string', 'The city must be a string');
+            // assert(typeof country === 'string', 'The country must be a string');
 
             assert(emailAdress.match(/^(([^<>()\[\]\\.,;:\s@']+(\.[^<>()\[\]\\.,;:\s@']+)*)|('.+'))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/), 'emailAdress is invalid');
             //8 karakters, 1 letter, 1 nummer en 1 speciaal teken
             assert(password.match(/^(?=.*[A-Za-z])(?=.*\d)(?=.*[@$!%*#?&])[A-Za-z\d@$!%*#?&]{8,}$/), 'password is invalid');
 
-            if (phoneNumber != undefined) {
-                assert(typeof phoneNumber === 'string', 'The phoneNumber must be a string');
-                assert(
-                    phoneNumber.match(
-                        /(06)(\s|\-|)\d{8}|31(\s6|\-6|6)\d{8}/
-                    ),
-                    'invalid phoneNumber'
-                )
-            }
+            // if (phoneNumber != undefined) {
+            //     assert(typeof phoneNumber === 'string', 'The phoneNumber must be a string');
+            //     assert(
+            //         phoneNumber.match(
+            //             /(06)(\s|\-|)\d{8}|31(\s6|\-6|6)\d{8}/
+            //         ),
+            //         'invalid phoneNumber'
+            //     )
+            // }
 
             next();
         } catch (err) {
