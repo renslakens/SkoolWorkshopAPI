938e7f9d9d18d6d80f7d4f44952830fda3f34415
Ayrianna <aechatlein@gmail.com>
2022-06-08 18:42:43 +0200

    Added token validation

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const router = express.Router();
 const userController = require("../controllers/user.controller");
+const aunthController = require("../controllers/auth.controller");
 
 //Add user
 router.post("/", userController.validateUser, userController.addUser);
@@ -18,10 +19,18 @@ router.post("/", userController.validateUser, userController.addUser);
 // router.put('/:id', authController.validateToken, userController.validateId, userController.validateUser, userController.updateUser);
 
 // Put route for accepting a new docent
-router.put('/:id', userController.validateId, userController.acceptUser);
+router.put(
+  "/:id",
+  aunthController.validateEmployeeToken,
+  userController.acceptUser
+);
 
 // //Delete routes for specific users
 // router.delete('/:id', authController.validateToken, userController.validateId, userController.deleteUser);
-router.delete("/:id", userController.validateId, userController.deleteUser);
+router.delete(
+  "/:id",
+  aunthController.validateEmployeeToken,
+  userController.deleteUser
+);
 
 module.exports = router;
