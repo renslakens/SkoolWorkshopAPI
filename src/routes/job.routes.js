5ad83a8b806455119301c5877a3306be12ad3dc9
Ayrianna <aechatlein@gmail.com>
2022-06-10 11:46:01 +0200

    Added routes to router

--- src/routes/job.routes.js
+++ src/routes/job.routes.js
@@ -2,7 +2,22 @@ const express = require("express");
 const router = express.Router();
 const jobController = require("../controllers/job.controller");
 
-module.exports = router;
+//Add job
+router.put("/", jobController.addJob);
+
+//Delete job
+router.delete("/:id", jobController.deleteJob);
+
+//Update job
+router.put("/:id", jobController.updateJob);
 
 //Get all open jobs
-router.get('/', jobController.getJobs);
+router.get("/", jobController.getJobs);
+
+//Accept job
+router.put("/moderate:id", jobController.acceptJob);
+
+//Deny job
+router.delete("/moderate:id", jobController.deleteTeacherFromJob);
+
+module.exports = router;
