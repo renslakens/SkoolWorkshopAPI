03bcaa29b537c32c264b6ad9e44ecce15092953e
Bramgelten <bramgelten@gmail.com>
2022-06-22 13:55:51 +0200

    fixed error in sql syntax

--- src/controllers/docent.controller.js
+++ src/controllers/docent.controller.js
@@ -111,7 +111,7 @@ let controller = {
     });
   },
   getDocent: (req, res) => {
-    const emailadres = req.body;
+    const emailadres = req.body.emailadres;
     pool.query(
       "SELECT * FROM docent WHERE loginEmail = ?;",
       [emailadres],
