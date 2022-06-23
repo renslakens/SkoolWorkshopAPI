0fdfba8476af19bbe85a9213f97cced965a92e21
AfroNeko <lisatyem@gmail.com>
2022-06-23 10:46:27 +0200

    Added get workshop route

--- src/routes/job.routes.js
+++ src/routes/job.routes.js
@@ -1,32 +1,35 @@
-const express = require("express");
-const router = express.Router();
-const jobController = require("../controllers/job.controller");
+const express = require('express')
+const router = express.Router()
+const jobController = require('../controllers/job.controller')
 
 //Add job
-router.post("/", jobController.addJob);
+router.post('/', jobController.addJob)
 
 //Delete job
-router.delete("/:id", jobController.deleteJob);
+router.delete('/:id', jobController.deleteJob)
 
 //Update job
-router.put("/:id", jobController.updateJob);
+router.put('/:id', jobController.updateJob)
 
 //Get all open jobs
-router.get("/", jobController.getJobs);
+router.get('/', jobController.getJobs)
+
+//Get workshops
+router.get('/workshops/', jobController.getWorkshops)
 
 //Get one job
-router.get("/:id", jobController.getJob);
+router.get('/:id', jobController.getJob)
 
 //Teacher added to job
-router.post("/moderate/", jobController.addTeacherToJob);
+router.post('/moderate/', jobController.addTeacherToJob)
 
 // //Apply to job
 // router.put("/apply/:emailadres", jobController.applyJob);
 
 //Accept job
-router.put("/moderate/:emailadres", jobController.acceptJob);
+router.put('/moderate/:emailadres', jobController.acceptJob)
 
 //Deny job
-router.delete("/moderate/:emailadres", jobController.deleteTeacherFromJob);
+router.delete('/moderate/:emailadres', jobController.deleteTeacherFromJob)
 
-module.exports = router;
+module.exports = router
