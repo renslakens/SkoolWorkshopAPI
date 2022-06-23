0203b20cf6121763134cf59aabc157bef22bc816
Bramgelten <bramgelten@gmail.com>
2022-06-23 14:06:54 +0200

    changed getjobs back after merge conflicts

--- src/controllers/job.controller.js
+++ src/controllers/job.controller.js
@@ -1,280 +1,284 @@
-const { query } = require('../../dbconnection')
-const pool = require('../../dbconnection')
-const logger = require('../config/config').logger
+const { query } = require("../../dbconnection");
+const pool = require("../../dbconnection");
+const logger = require("../config/config").logger;
 
 let controller = {
-    addJob: (req, res) => {
-        let job = req.body
-        let startTijd = new Date(job.startTijd)
-            .toISOString()
-            .replace('T', ' ')
-            .slice(0, 19)
+  addJob: (req, res) => {
+    let job = req.body;
+    let startTijd = new Date(job.startTijd)
+      .toISOString()
+      .replace("T", " ")
+      .slice(0, 19);
 
-        let eindTijd = new Date(job.eindTijd)
-            .toISOString()
-            .replace('T', ' ')
-            .slice(0, 19)
+    let eindTijd = new Date(job.eindTijd)
+      .toISOString()
+      .replace("T", " ")
+      .slice(0, 19);
 
-        let valuesJob = [
-            [
-                job.aantalDocenten,
-                startTijd,
-                eindTijd,
-                job.locatieID,
-                job.workshopID,
-                job.klantID,
-                job.doelgroepID,
-            ],
-        ]
+    let valuesJob = [
+      [
+        job.aantalDocenten,
+        startTijd,
+        eindTijd,
+        job.locatieID,
+        job.workshopID,
+        job.klantID,
+        job.doelgroepID,
+      ],
+    ];
 
-        pool.query(
-            'INSERT INTO Opdracht (aantalDocenten, startTijd, eindTijd, locatieID, workshopID, klantID, doelgroepID) VALUES ?;', [valuesJob],
-            function(error, result) {
-                if (error) {
-                    logger.debug(error.sqlMessage)
-                    res.status(400).json({
-                        status: 400,
-                        message: 'Oeps, er is iets fout gegaan ' + error,
-                    })
-                } else {
-                    res.status(201).json({
-                        status: 201,
-                        message: 'Opdracht is toegevoegd',
-                    })
-                }
-            },
-        )
-    },
-    deleteJob: (req, res) => {
-        const jobID = req.params.id
-
-        pool.query('DELETE FROM Opdracht WHERE jobID = ?;', [jobID], function(
-            error,
-            result,
-        ) {
-            if (error) {
-                logger.debug(error.sqlMessage)
-                res.statur(400).json({
-                    status: 400,
-                    message: error,
-                })
-            }
-            if (result.affectedRows > 0) {
-                res.status(200).json({
-                    status: 200,
-                    message: `Opdracht met ID ${jobID} is verwijderd`,
-                })
-            } else {
-                res.status(400).json({
-                    status: 400,
-                    message: 'Deze opdracht bestaat niet',
-                })
-            }
-        })
-    },
-    updateJob: (req, res) => {
-        const jobID = req.params.id
-        let job = req.body
-        let startTijd = new Date(job.startTijd)
-            .toISOString()
-            .replace('T', ' ')
-            .slice(0, 19)
-
-        let eindTijd = new Date(job.eindTijd)
-            .toISOString()
-            .replace('T', ' ')
-            .slice(0, 19)
-
-        pool.query(
-            'UPDATE Opdracht SET aantalDocenten=?, startTijd=?, eindTijd=? WHERE jobID =?;', [job.aantalDocenten, startTijd, eindTijd, jobID],
-            function(error, result) {
-                if (error) {
-                    res.status(400).json({
-                        status: 400,
-                        message: error,
-                    })
-                }
-                if (result.affectedRows > 0) {
-                    res.status(200).json({
-                        status: 200,
-                        message: `Opdracht met ID ${jobID} is gewijzigd`,
-                    })
-                } else {
-                    res.status(400).json({
-                        status: 400,
-                        message: 'Deze opdracht bestaat niet',
-                    })
-                }
-            },
-        )
-    },
-    getJobs: (req, res) => {
-        let queryString =
-            'SELECT O.opdrachtID, W.workshopnaam, W.beschrijving, O.aantalDocenten, O.startTijd, O.eindTijd, L.naam, L.land, L.postcode, L.straat, L.huisnummer, L.plaats, O.locatieID, O.workshopID, O.klantID, O.doelgroepID FROM workshop W, Opdracht O, locatie L WHERE O.workshopID IN (SELECT O.workshopID FROM docentinopdracht DIO WHERE DIO.opdrachtID = O.opdrachtID AND DIO.isBevestigd = TRUE HAVING COUNT(DIO.opdrachtID) < O.aantalDocenten );'
-        pool.query(queryString, function(error, results, fields) {
-            if (error) {
-                res.status(400).json({
-                    status: 400,
-                    message: error,
-                })
-            }
-            if (results.length > 0) {
-                res.status(200).json({
-                    status: 200,
-                    result: results,
-                })
-            }
-        })
-    },
-    acceptJob: (req, res, next) => {
-        //let emailadres = req.params.emailadres
-        const { loginEmail, opdrachtID } = req.query
-
-        let queryString = 'UPDATE DocentInOpdracht SET isBevestigd = 1';
-
-        if (loginEmail || opdrachtID) {
-            queryString += ' WHERE '
-            if (loginEmail) {
-                queryString += `loginEmail = '${loginEmail}'`
-            }
-            if (loginEmail && opdrachtID) {
-                queryString += ' AND '
-            }
-            if (opdrachtID) {
-                queryString += `opdrachtID='${opdrachtID}'`
-            }
+    pool.query(
+      "INSERT INTO Opdracht (aantalDocenten, startTijd, eindTijd, locatieID, workshopID, klantID, doelgroepID) VALUES ?;",
+      [valuesJob],
+      function (error, result) {
+        if (error) {
+          logger.debug(error.sqlMessage);
+          res.status(400).json({
+            status: 400,
+            message: "Oeps, er is iets fout gegaan " + error,
+          });
+        } else {
+          res.status(201).json({
+            status: 201,
+            message: "Opdracht is toegevoegd",
+          });
         }
-        logger.debug(queryString)
+      }
+    );
+  },
+  deleteJob: (req, res) => {
+    const jobID = req.params.id;
 
-        pool.query(
-            queryString,
-            function(error, results) {
-                if (error) {
-                    next(error)
-                }
-
-                // logger.debug("#results =", results.length);
-                res.status(200).json({
-                    status: 200,
-                    message: "Docent is toegevoegd aan workshop",
-                })
-            },
-        )
-    },
-
-    getWorkshops: (req, res, next) => {
-        const { loginEmail, isBevestigd } = req.query
-        logger.debug(`loginEmail = ${loginEmail} isBevestigd = ${isBevestigd}`)
-
-        let queryString = 'SELECT O.opdrachtID, W.workshopnaam, W.beschrijving, O.aantalDocenten, O.startTijd, O.eindTijd, L.naam, L.land, L.postcode, L.straat, L.huisnummer, L.plaats, O.locatieID, O.workshopID, O.klantID, O.doelgroepID, DIO.loginEmail, DIO.isBevestigd FROM workshop W, Opdracht O, locatie L, DocentInOpdracht DIO'
-
-        if (loginEmail || isBevestigd) {
-            queryString += ' WHERE '
-            if (loginEmail) {
-                queryString += `loginEmail = '${loginEmail}'`
-            }
-            if (loginEmail && isBevestigd) {
-                queryString += ' AND '
-            }
-            if (isBevestigd) {
-                queryString += `isBevestigd='${isBevestigd}'`
-            }
+    pool.query(
+      "DELETE FROM Opdracht WHERE jobID = ?;",
+      [jobID],
+      function (error, result) {
+        if (error) {
+          logger.debug(error.sqlMessage);
+          res.statur(400).json({
+            status: 400,
+            message: error,
+          });
         }
-        logger.debug(queryString)
+        if (result.affectedRows > 0) {
+          res.status(200).json({
+            status: 200,
+            message: `Opdracht met ID ${jobID} is verwijderd`,
+          });
+        } else {
+          res.status(400).json({
+            status: 400,
+            message: "Deze opdracht bestaat niet",
+          });
+        }
+      }
+    );
+  },
+  updateJob: (req, res) => {
+    const jobID = req.params.id;
+    let job = req.body;
+    let startTijd = new Date(job.startTijd)
+      .toISOString()
+      .replace("T", " ")
+      .slice(0, 19);
 
-        pool.query(queryString, function(error, results, fields) {
-            // Handle error after the release.
-            if (error) {
-                next(error)
-            }
+    let eindTijd = new Date(job.eindTijd)
+      .toISOString()
+      .replace("T", " ")
+      .slice(0, 19);
 
-            // logger.debug("#results =", results.length);
-            res.status(200).json({
-                status: 200,
-                result: results,
-            })
-        })
-    },
+    pool.query(
+      "UPDATE Opdracht SET aantalDocenten=?, startTijd=?, eindTijd=? WHERE jobID =?;",
+      [job.aantalDocenten, startTijd, eindTijd, jobID],
+      function (error, result) {
+        if (error) {
+          res.status(400).json({
+            status: 400,
+            message: error,
+          });
+        }
+        if (result.affectedRows > 0) {
+          res.status(200).json({
+            status: 200,
+            message: `Opdracht met ID ${jobID} is gewijzigd`,
+          });
+        } else {
+          res.status(400).json({
+            status: 400,
+            message: "Deze opdracht bestaat niet",
+          });
+        }
+      }
+    );
+  },
+  getJobs: (req, res) => {
+    const loginEmail = req.params.emailadres;
+    let queryString =
+      "SELECT * FROM jobslist WHERE opdrachtID != ANY(SELECT opdrachtID FROM docentinopdracht WHERE loginEmail = ?);";
+    pool.query(queryString, [loginEmail], function (error, results, fields) {
+      if (error) {
+        res.status(400).json({
+          status: 400,
+          message: error,
+        });
+      }
+      if (results.length > 0) {
+        res.status(200).json({
+          status: 200,
+          result: results,
+        });
+      }
+    });
+  },
+  acceptJob: (req, res, next) => {
+    //let emailadres = req.params.emailadres
+    const { loginEmail, opdrachtID } = req.query;
 
-    addTeacherToJob: (req, res) => {
-        const teacherJob = req.body
+    let queryString = "UPDATE DocentInOpdracht SET isBevestigd = 1";
 
-        pool.query(
-            'INSERT INTO DocentInOpdracht (loginEmail, opdrachtID) VALUES (?,?);', [teacherJob.emailadres, teacherJob.id],
-            function(error, result) {
-                if (error) throw error
+    if (loginEmail || opdrachtID) {
+      queryString += " WHERE ";
+      if (loginEmail) {
+        queryString += `loginEmail = '${loginEmail}'`;
+      }
+      if (loginEmail && opdrachtID) {
+        queryString += " AND ";
+      }
+      if (opdrachtID) {
+        queryString += `opdrachtID='${opdrachtID}'`;
+      }
+    }
+    logger.debug(queryString);
 
-                if (result.affectedRows > 0) {
-                    res.status(200).json({
-                        status: 200,
-                        message: 'Docent is toegevoegd aan de opdracht',
-                    })
-                } else {
-                    res.status(400).json({
-                        status: 400,
-                        message: `Docent bestaat niet`,
-                    })
-                }
-            },
-        )
-    },
-    deleteTeacherFromJob: (req, res) => {
-        const docentID = req.params.id
+    pool.query(queryString, function (error, results) {
+      if (error) {
+        next(error);
+      }
 
-        pool.query(
-            'DELETE FROM DocentInOpdracht WHERE docentID = ?', [docentID],
-            function(error, result) {
-                if (error) throw error
+      // logger.debug("#results =", results.length);
+      res.status(200).json({
+        status: 200,
+        message: "Docent is toegevoegd aan workshop",
+      });
+    });
+  },
 
-                if (result.affectedRows > 0) {
-                    res.status(200).json({
-                        status: 200,
-                        message: 'Docent is van de opdracht verwijderd.',
-                    })
-                } else {
-                    res.status(400).json({
-                        status: 400,
-                        message: `Docent bestaat niet`,
-                    })
-                }
-            },
-        )
-    },
-    getJob: (req, res, next) => {
-        const jobID = req.params.id
-        logger.debug(`Job with ID ${jobID} is requested`)
-        let queryString =
-            'SELECT W.workshopnaam, O.startTijd, O.eindTijd, L.naam FROM Workshop W, Opdracht O, Locatie L WHERE opdrachtID =?'
-        pool.query(queryString, [jobID], function(error, results, fields) {
-            // if (error) {
-            //     res.status(400).json({
-            //         status: 400,
-            //         message: error,
-            //     })
-            // }
-            // if (results.length > 0) {
-            //     res.status(200).json({
-            //         status: 200,
-            //         result: results,
-            //     })
-            // } else {
-            //     res.status(400).json({
-            //         status: 400,
-            //         message: 'Er zijn geen openstaande opdrachten',
-            //     })
-            // }
+  getWorkshops: (req, res, next) => {
+    const { loginEmail, isBevestigd } = req.query;
+    logger.debug(`loginEmail = ${loginEmail} isBevestigd = ${isBevestigd}`);
 
-            if (error) {
-                next(error)
-            }
+    let queryString =
+      "SELECT O.opdrachtID, W.workshopnaam, W.beschrijving, O.aantalDocenten, O.startTijd, O.eindTijd, L.naam, L.land, L.postcode, L.straat, L.huisnummer, L.plaats, O.locatieID, O.workshopID, O.klantID, O.doelgroepID, DIO.loginEmail, DIO.isBevestigd FROM workshop W, Opdracht O, locatie L, DocentInOpdracht DIO";
 
-            // logger.debug("#results =", results.length);
-            res.status(200).json({
-                status: 200,
-                result: results,
-            })
-        })
-    },
-}
+    if (loginEmail || isBevestigd) {
+      queryString += " WHERE ";
+      if (loginEmail) {
+        queryString += `loginEmail = '${loginEmail}'`;
+      }
+      if (loginEmail && isBevestigd) {
+        queryString += " AND ";
+      }
+      if (isBevestigd) {
+        queryString += `isBevestigd='${isBevestigd}'`;
+      }
+    }
+    logger.debug(queryString);
 
-module.exports = controller
+    pool.query(queryString, function (error, results, fields) {
+      // Handle error after the release.
+      if (error) {
+        next(error);
+      }
+
+      // logger.debug("#results =", results.length);
+      res.status(200).json({
+        status: 200,
+        result: results,
+      });
+    });
+  },
+
+  addTeacherToJob: (req, res) => {
+    const teacherJob = req.body;
+
+    pool.query(
+      "INSERT INTO DocentInOpdracht (loginEmail, opdrachtID) VALUES (?,?);",
+      [teacherJob.emailadres, teacherJob.id],
+      function (error, result) {
+        if (error) throw error;
+
+        if (result.affectedRows > 0) {
+          res.status(200).json({
+            status: 200,
+            message: "Docent is toegevoegd aan de opdracht",
+          });
+        } else {
+          res.status(400).json({
+            status: 400,
+            message: `Docent bestaat niet`,
+          });
+        }
+      }
+    );
+  },
+  deleteTeacherFromJob: (req, res) => {
+    const docentID = req.params.id;
+
+    pool.query(
+      "DELETE FROM DocentInOpdracht WHERE docentID = ?",
+      [docentID],
+      function (error, result) {
+        if (error) throw error;
+
+        if (result.affectedRows > 0) {
+          res.status(200).json({
+            status: 200,
+            message: "Docent is van de opdracht verwijderd.",
+          });
+        } else {
+          res.status(400).json({
+            status: 400,
+            message: `Docent bestaat niet`,
+          });
+        }
+      }
+    );
+  },
+  getJob: (req, res, next) => {
+    const jobID = req.params.id;
+    logger.debug(`Job with ID ${jobID} is requested`);
+    let queryString =
+      "SELECT W.workshopnaam, O.startTijd, O.eindTijd, L.naam FROM Workshop W, Opdracht O, Locatie L WHERE opdrachtID =?";
+    pool.query(queryString, [jobID], function (error, results, fields) {
+      // if (error) {
+      //     res.status(400).json({
+      //         status: 400,
+      //         message: error,
+      //     })
+      // }
+      // if (results.length > 0) {
+      //     res.status(200).json({
+      //         status: 200,
+      //         result: results,
+      //     })
+      // } else {
+      //     res.status(400).json({
+      //         status: 400,
+      //         message: 'Er zijn geen openstaande opdrachten',
+      //     })
+      // }
+
+      if (error) {
+        next(error);
+      }
+
+      // logger.debug("#results =", results.length);
+      res.status(200).json({
+        status: 200,
+        result: results,
+      });
+    });
+  },
+};
+
+module.exports = controller;
